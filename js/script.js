f3a00d6e7f78d9a6a0a18b814555a04c25a8498b
Claudio <claudiodicaprio89@gmail.com>
2021-11-29 16:28:27 +0100

    methods

--- js/script.js
+++ js/script.js
@@ -1,31 +1,50 @@
-const app = new Vue({
-    el:'#root',
-    data:{
-
-    },
-    methods:{
-        newTask: {
-            text: '',
-            done: false
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            newTask: {
+                text: '',
+                done: false
+            },
+            tasks: [
+                {
+                    text: 'Bring luky for a walk',
+                    done: false
+                },
+                {
+                    text: 'Repeat js',
+                    done: false
+                },
+                {
+                    text: 'trainning',
+                    done: false
+                },
+                {
+                    text: 'practice tabled faro shuffle',
+                    done: false
+                },
+                
+            ]
         },
-        tasks: [
-            {
-                text: 'Bring luky for a walk',
-                done: false
+
+        methods: {
+
+            add(){
+                if (this.newTask.text[0] !== ' ' && this.newTask.text.length > 0) {
+                    this.tasks.push(this.newTask);
+                    this.newTask = {text: '',done: false};
+                }
             },
-            {
-                text: 'Repeat js',
-                done: false
+            removeTask(index){
+                this.tasks.splice(index, 1);
             },
-            {
-                text: 'trainning',
-                done: false
-            },
-            {
-                text: 'practice tabled faro shuffle',
-                done: false
-            },
-            
-        ]
+            doneTask(index){
+                if(this.tasks[index].done === true) {
+                    this.tasks[index].done = false;
+                } else {
+                    this.tasks[index].done = true;
+                }                
+            }
+        }
     }
-})
+);
