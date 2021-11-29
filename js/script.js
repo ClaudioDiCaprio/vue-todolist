bb7df68d900dc6f701ecc0ec9318e40328af5590
Claudio <claudiodicaprio89@gmail.com>
2021-11-29 15:52:54 +0100

    js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,28 @@ const app = new Vue({
 
     },
     methods:{
-        
+        newTask: {
+            text: '',
+            done: false
+        },
+        tasks: [
+            {
+                text: 'Bring luky for a walk',
+                done: false
+            },
+            {
+                text: 'Repeat js',
+                done: false
+            },
+            {
+                text: 'trainning',
+                done: false
+            },
+            {
+                text: 'practice tabled faro shuffle',
+                done: false
+            },
+            
+        ]
     }
 })
